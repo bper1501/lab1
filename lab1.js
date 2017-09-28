982e691602c0be497702825e536194caaa3117b9
Perez <perbraa@dunwoody.edu>
2017-09-27 21:08:27 -0500

    lab1 push over

---
+++ lab1.js
@@ -0,0 +1,179 @@
+var worldCup = [
+  {
+    "Country": "Italy",
+    "WorldCupWon": 4,
+    "wcQualified": "18 of 20",
+    "BestRound": "Final",
+    "FIFA RANK": 12
+  },
+  {
+    "Country": "Germany",
+    "WorldCupWon": 4,
+    "wcQualified": "18 of 20",
+    "BestRound": "Final",
+    "FIFA RANK": 2
+  },
+  {
+    "Country": "Uruguay",
+    "WorldCupWon": 2,
+    "wcQualified": "12 of 20",
+    "BestRound": "Final",
+    "FIFA RANK": 17
+  },
+  {
+    "Country": "Argentina",
+    "WorldCupWon": 2,
+    "wcQualified": "16 of 20",
+    "BestRound": "Final",
+    "FIFA RANK": 3
+  },
+  {
+    "Country": "England",
+    "WorldCupWon": 1,
+    "wcQualified": "14 of 20",
+    "BestRound": "Final",
+    "FIFA RANK": 13
+  },
+  {
+    "Country": "France",
+    "WorldCupWon": 1,
+    "wcQualified": "14 of 20",
+    "BestRound": "Final",
+    "FIFA RANK": 10
+  },
+  {
+    "Country": "Spain",
+    "WorldCupWon": 1,
+    "wcQualified": "14 of 20",
+    "BestRound": "Final",
+    "FIFA RANK": 11
+  },
+  {
+    "Country": "Switzerland",
+    "WorldCupWon": 0,
+    "wcQualified": "10 of 20",
+    "BestRound": "Quarter",
+    "FIFA RANK": 4
+  },
+  {
+    "Country": "Poland",
+    "WorldCupWon": 0,
+    "wcQualified": "7 of 20",
+    "BestRound": "Third",
+    "FIFA RANK": 5
+  },
+  {
+    "Country": "Portugal",
+    "WorldCupWon": 0,
+    "wcQualified": "6 of 20",
+    "BestRound": "Third",
+    "FIFA RANK": 6
+  },
+  {
+    "Country": "Chile",
+    "WorldCupWon": 0,
+    "wcQualified": "9 of 20",
+    "BestRound": "Third",
+    "FIFA RANK": 7
+  },
+  {
+    "Country": "Colombia",
+    "WorldCupWon": 0,
+    "wcQualified": "5 of 20",
+    "BestRound": "Quarter",
+    "FIFA RANK": 8
+  },
+  {
+    "Country": "Belgium",
+    "WorldCupWon": 0,
+    "wcQualified": "13 of 20",
+    "BestRound": "Fourth",
+    "FIFA RANK": 9
+  },
+  {
+    "Country": "Mexico",
+    "WorldCupWon": 0,
+    "wcQualified": "15 of 20",
+    "BestRound": "Quarter",
+    "FIFA RANK": 14
+  },
+  {
+    "Country": "Peru",
+    "WorldCupWon": 0,
+    "wcQualified": "4 of 20",
+    "BestRound": "Quarter",
+    "FIFA RANK": 15
+  },
+  {
+    "Country": "Croatia",
+    "WorldCupWon": 0,
+    "wcQualified": "4 of 6(12/20)",
+    "BestRound": "Third",
+    "FIFA RANK": 16
+  },
+  {
+    "Country": "Wales",
+    "WorldCupWon": 0,
+    "wcQualified": "1 of 20",
+    "BestRound": "Quarter",
+    "FIFA RANK": 18
+  },
+  {
+    "Country": "Sweden",
+    "WorldCupWon": 0,
+    "wcQualified": "11 of 20",
+    "BestRound": "Runner",
+    "FIFA RANK": 19
+  },
+  {
+    "Country": "Iceland",
+    "WorldCupWon": 0,
+    "wcQualified": "0 of 0",
+    "BestRound": "N/A",
+    "FIFA RANK": 20
+  },
+  {
+    "Country": "Costa Rica",
+    "WorldCupWon": 0,
+    "wcQualified": "4 of 20",
+    "BestRound": "Quarter",
+    "FIFA RANK": 21
+  },
+  {
+    "Country": "Netherland",
+    "WorldCupWon": 0,
+    "wcQualified": "10 of 20",
+    "BestRound": "Runner",
+    "FIFA RANK": 36
+  },
+  {
+    "Country": "Japan",
+    "WorldCupWon": 0,
+    "wcQualified": "5 of 20",
+    "BestRound": "RO16",
+    "FIFA RANK": 44
+  },
+  {
+    "Country": "Turkey",
+    "WorldCupWon": 0,
+    "wcQualified": "2 of 20",
+    "BestRound": "Third",
+    "FIFA RANK": 33
+  },
+  {
+    "Country": "USA",
+    "WorldCupWon": 0,
+    "wcQualified": "10 of 20",
+    "BestRound": "Semis",
+    "FIFA RANK": 26
+  },
+  {
+    "Country": "Brazil",
+    "WorldCupWon": 5,
+    "wcQualif": "20 of 20",
+    "BestRound": "Final",
+    "FIFA RANK": 1
+  }
+]
+
+
